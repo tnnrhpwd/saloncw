c90ecd93a6c6d8befbd4bd284a360fce3e4f1c43
Steven Tanner Hopwood <65215097+tnnrhpwd@users.noreply.github.com>
2022-05-14 10:27:30 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Appointment from './components/Appointment';
+import Instagram from './components/Instagram';
+import Links from './components/Links';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+       insert img
+      <Appointment/>
+      <Instagram/>
+      <Links/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import logo from './../assets/Logo - White-Nosides-transparent512.png'
+import fbimg from './../assets/facebook.png'
+import igimg from './../assets/instagram.png'
+
+function Footer() {
+  // Get browser year for copyright
+  var today = new Date();
+  var year = today.getFullYear();
+
+  return (
+    <div className='footer'>
+      <img className='footer-img' src={logo} alt="cw logo" />
+      <div className='footer-copyright'>
+        © {year}
+      </div>
+      <div className='footer-links'>
+        <img className='footer-links-fb' src={fbimg} alt='facebook logo'/>
+        <div className='footer-links-ig-bg'>
+          <img className='footer-links-ig' src={igimg} alt='instagram logo'/>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
